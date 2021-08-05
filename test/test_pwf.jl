130bfcb7c6769d0b7fcf07c7620c01e63ecd80ba
pedrohamacher <pedrohamacher@gmail.com>
2021-08-05 11:19:10 -0300

    Test bus and branch conversion

--- test/test_pwf.jl
+++ test/test_pwf.jl
@@ -127,6 +127,88 @@ end
 
 @testset "Dict to PowerModels" begin
     @testset "Intermediary functions" begin
+        file = open(joinpath(@__DIR__,"data/sistema_teste_radial.pwf"))
+        pwf_data = ParsePWF._parse_pwf_data(file)
+        pm_data = Dict{String, Any}()
+
+        @testset "Bus" begin
+            ParsePWF._pwf2pm_bus!(pm_data, pwf_data)
+            
+            @test haskey(pm_data, "bus")
+            @test length(pm_data["bus"]) == 9
+
+            for (idx, bus) in pm_data["bus"]
+                @test haskey(bus, "zone")
+                @test haskey(bus, "bus_i")
+                @test haskey(bus, "bus_type")
+                @test haskey(bus, "name")
+                @test haskey(bus, "vmax")
+                @test haskey(bus, "source_id")
+                @test haskey(bus, "area")
+                @test haskey(bus, "vmin")
+                @test haskey(bus, "index")
+                @test haskey(bus, "va")
+                @test haskey(bus, "vm")
+                @test haskey(bus, "base_kv")
+
+                @test isa(bus["zone"], Int)
+                @test isa(bus["bus_i"], Int)
+                @test isa(bus["bus_type"], Int)
+                @test isa(bus["name"], String)
+                @test isa(bus["vmax"], Float64)
+                @test isa(bus["source_id"], Vector)
+                @test isa(bus["area"], Int)
+                @test isa(bus["vmin"], Float64)
+                @test isa(bus["index"], Int)
+                @test isa(bus["va"], Float64)
+                @test isa(bus["vm"], Float64)
+                @test isa(bus["base_kv"], Float64)
+            end
+        end
+
+        @testset "Branch" begin
+            ParsePWF._pwf2pm_branch!(pm_data, pwf_data)
+            
+            @test haskey(pm_data, "branch")
+            @test length(pm_data["branch"]) == 7
+
+            for (idx, branch) in pm_data["branch"]
+                @test haskey(branch, "br_r")
+                @test haskey(branch, "shift")
+                @test haskey(branch, "br_x")
+                @test haskey(branch, "g_to")
+                @test haskey(branch, "g_fr")
+                @test haskey(branch, "b_fr")
+                @test haskey(branch, "source_id")
+                @test haskey(branch, "f_bus")
+                @test haskey(branch, "br_status")
+                @test haskey(branch, "t_bus")
+                @test haskey(branch, "b_to")
+                @test haskey(branch, "index")
+                @test haskey(branch, "angmin")
+                @test haskey(branch, "angmax")
+                @test haskey(branch, "transformer")
+                @test haskey(branch, "tap")
+
+                @test isa(branch["br_r"], Float64)
+                @test isa(branch["shift"], Float64)
+                @test isa(branch["br_x"], Float64)
+                @test isa(branch["g_to"], Float64)
+                @test isa(branch["g_fr"], Float64)
+                @test isa(branch["b_fr"], Float64)
+                @test isa(branch["source_id"], Vector)
+                @test isa(branch["f_bus"], Int)
+                @test isa(branch["br_status"], Int)
+                @test isa(branch["t_bus"], Int)
+                @test isa(branch["b_to"], Float64)
+                @test isa(branch["index"], Int)
+                @test isa(branch["angmin"], Float64)
+                @test isa(branch["angmax"], Float64)
+                @test isa(branch["transformer"], Bool)
+                @test isa(branch["tap"], Float64)
+            end
+
+        end
 
     end
 
