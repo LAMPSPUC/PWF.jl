aa223af7d916c3a684e770f2b8d1436e137832ad
pedrohamacher <pedrohamacher@gmail.com>
2021-09-01 10:01:48 -0300

    Fix file calls in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
-using Test, PowerModels, Ipopt
+using Ipopt
+using Test
+using PowerModels
 
-include("src/ParserPWF.jl")
+include("../src/ParserPWF.jl")
 
 @testset "Test functions" begin
-    include("test/test_functions.jl")
-    include("test/test_pwf.jl")
+    include("test_functions.jl")
+    include("test_pwf.jl")
 end
