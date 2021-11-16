f16d37bd994ae86e69c485aa206341d20ece7368
pedrohamacher <pedrohamacher@gmail.com>
2021-11-16 15:18:44 -0300

    Rename 'info' fields to 'actions' and 'parameters'

--- src/pwf2pm/info.jl
+++ src/pwf2pm/info.jl
@@ -1,22 +1,21 @@
 function _pwf2pm_info!(pm_data::Dict, pwf_data::Dict, option::String, status::Char, section::String)
     key = lowercase(option)
     value = status == 'L' ? true : status == 'D' ? false : error("Execution option $key status not defined")
-    pm_data["info"][section][key] = value
+    pm_data["info"]["actions"][key] = value
 end
 
 function _pwf2pm_info!(pm_data::Dict, pwf_data::Dict, option::String, value::Real, section::String)
     key = lowercase(option)
-    pm_data["info"][section][key] = value
+    pm_data["info"]["parameters"][key] = value
 end
 
 function _pwf2pm_info!(pm_data::Dict, pwf_data::Dict)
-    pm_data["info"] = Dict{String,Dict{String,Any}}()
+    pm_data["info"] = Dict("actions" => Dict{String,Any}(), "parameters" => Dict{String,Any}())
     info_sections = ["DOPC", "DOPC IMPR", "DCTE"]
 
     for section in info_sections
         if haskey(pwf_data, section)
 
-            pm_data["info"][section] = Dict{String,Any}()
             for (option,value) in pwf_data[section]
                 _pwf2pm_info!(pm_data, pwf_data, option, value, section)
             end
